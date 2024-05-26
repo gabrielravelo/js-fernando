973d9206ce5064a68c9d80eca2a0cb38731e9fb0
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-05-26 14:34:58 -0400

    http

---
+++ 07-http-app/src/rickandmorty/rickandmorty-app.js
@@ -0,0 +1,18 @@
+
+/**
+ * @returns {Object} quote information
+ */
+const fechtQuote = () => {
+    
+};
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const RickAndMortyApp = ( element ) => {
+
+    document.querySelector('#app-title').innerHTML = 'RickAndMorty App';
+    element.innerHTML = 'Loading...';
+ 
+};
